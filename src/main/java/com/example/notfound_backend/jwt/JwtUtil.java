69e828ada6c2acaeadc3cbc88acc55c3df62d6ae
jojeongtae <jeongtae9324@gmail.com>
2025-07-16 12:54:18 +0900

    jwt

---
+++ src/main/java/com/example/notfound_backend/jwt/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.example.notfound_backend.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private SecretKey secretKey;
+
+    public JwtUtil(@Value("jwt.secret.key") String secretKey) {
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    }
+
+    public String createToken(String category, String username, String role, Long expiredMs) {
+        return Jwts.builder()
+                .claim("category", category)
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+
+    }
+
+    public String getUserName(String token) {
+        return Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token).getPayload().get("username").toString();
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token).getPayload().get("role").toString();
+    }
+
+    public String getCategory(String token) {
+        return Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token).getPayload().get("category",String.class);
+    }
+
+    public boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+}
