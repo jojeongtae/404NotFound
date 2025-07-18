24f043ef9e1abadd2cdef1e0e8ec6f48545e6f57
choi-seonah <dondo0412@naver.com>
2025-07-18 18:07:23 +0900

    survey answers

---
+++ src/main/java/com/example/notfound_backend/data/dto/SurveyAnswerDTO.java
@@ -0,0 +1,20 @@
+package com.example.notfound_backend.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SurveyAnswerDTO {
+    private Integer id;
+    private String username;
+    private String answers;
+    private Instant createdAt;
+    private Integer parentId;
+}
