8efe4857bf11a7d1094febe68a3840a4d05eb23d
jojeongtae <jeongtae9324@gmail.com>
2025-07-24 09:32:05 +0900

    dd

--- src/main/java/com/example/notfound_backend/data/dao/UploadImageDAO.java
+++ src/main/java/com/example/notfound_backend/data/dao/UploadImageDAO.java
@@ -1,18 +1,19 @@
-package com.example.notfound_backend.service;
-
-import com.example.notfound_backend.data.entity.UploadedImageEntity;
-import com.example.notfound_backend.data.repository.UploadedImageRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class ImageMetadataService { // DB 저장 전담
-    private final UploadedImageRepository uploadedImageRepository;
-
-    public void save(UploadedImageEntity entity) {
-        uploadedImageRepository.save(entity);
-    }
-
-
-}
+//package com.example.notfound_backend.service;
+//
+//import com.example.notfound_backend.data.entity.UploadedImageEntity;
+//import com.example.notfound_backend.data.repository.UploadedImageRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class ImageMetadataService { // DB 저장 전담
+//    private final UploadedImageRepository uploadedImageRepository;
+//
+//    public void save(UploadedImageEntity entity) {
+//        uploadedImageRepository.save(entity);
+//
+//    }
+//
+//}
+//
