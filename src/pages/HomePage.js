557f57d1e7f1ab6a3e78d92b9342171517c420fa
jojeongtae <jeongtae9324@gmail.com>
2025-07-29 18:09:34 +0900

    homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RankingPage from './RankingPage'; // RankingPage 컴포넌트 임포트
 
 const HomePage = () => {
     return (
@@ -6,6 +7,14 @@ const HomePage = () => {
             <h1>환영합니다!</h1>
             <p>이곳은 404NotFound의 메인 페이지입니다.</p>
             <p>왼쪽 게시판 목록을 통해 이동해주세요.</p>
+            <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+                <div style={{ flex: 1, marginRight: '10px' }}>
+                    <RankingPage type="recommend" />
+                </div>
+                <div style={{ flex: 1, marginLeft: '10px' }}>
+                    <RankingPage type="comment" />
+                </div>
+            </div>
         </div>
     );
 };
