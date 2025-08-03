2eddb1c37930fb12256fe0bb62b2b667359640e3
jojeongtae <jeongtae9324@gmail.com>
2025-08-03 15:28:51 +0900

    ?????

--- src/main/java/com/example/notfound_backend/data/dto/login/OAuth2Response.java
+++
@@ -1,8 +0,0 @@
-package com.example.notfound_backend.data.dto.login;
-
-public interface OAuth2Response {
-    String getProvider();
-    String getProviderId();
-    String getEmail();
-    String getName();
-}
