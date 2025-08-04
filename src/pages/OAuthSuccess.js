46679d0b2e7168564f97de7f5aef0e8b222835b8
jojeongtae <jeongtae9324@gmail.com>
2025-08-04 13:14:04 +0900

    front

--- src/pages/OAuthSuccess.js
+++ src/pages/OAuthSuccess.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setToken } from '../features/auth/tokenSlice';
+import { setUser } from '../features/auth/userSlice';
 import { useNavigate } from 'react-router-dom';
 
 const OAuthSuccess = () => {
@@ -10,9 +11,24 @@ const OAuthSuccess = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const accessToken = params.get('accessToken');
+    const username = params.get('username');
+    const nickname = params.get('nickname');
+    const role = params.get('role');
 
-    if (accessToken) {
+    if (accessToken && username) {
+      // JWT 저장
       dispatch(setToken('Bearer ' + accessToken));
+
+      // 유저 정보 저장
+      dispatch(setUser({
+        username,
+        nickname: nickname || '',
+        role: role || 'ROLE_USER',
+      }));
+
+      // URL 정리
+      window.history.replaceState({}, document.title, '/');
+
       navigate('/'); // 메인 페이지로 이동
     } else {
       navigate('/login?error'); // 실패 시 로그인 화면으로
