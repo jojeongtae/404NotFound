fd97ab059d3d5378e9a53380920b8d931fdd3f6f
jojeongtae <jeongtae9324@gmail.com>
2025-08-05 15:06:42 +0900

    img

--- src/main/java/com/example/notfound_backend/configure/WebConfig.java
+++ src/main/java/com/example/notfound_backend/configure/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://404notfoundpage.duckdns.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true)
                 .allowedHeaders("*");
