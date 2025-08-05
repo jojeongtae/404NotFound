c3e0e5742d8d0c616676d82d4e0fd449d0b57d53
jangs-uxui <jangs.uxui@gmail.com>
2025-08-05 14:34:35 +0900

    board-view css align

--- src/features/auth/LoginForm.js
+++ src/features/auth/LoginForm.js
@@ -135,8 +135,8 @@ const LoginForm = ({ onClose }) => {
         </div>
       </form>
       <div className="oauth2_wrap">
-        <button type='button' onClick={handleKakaoLogin} className="kakao">카카오 로그인</button>
-        <button type='button' onClick={handleNaverLogin} className="naver">
+        <button type='button' onClick={handleKakaoLogin} className="btn kakao">카카오 로그인</button>
+        <button type='button' onClick={handleNaverLogin} className="btn naver">
           <span className="svg-wrap">
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M16.273 12.845 7.376 0H0v24h7.727V11.155L16.624 24H24V0h-7.727v12.845z"></path>
